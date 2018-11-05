91d031ed8209058f141a283e009489a0d5072877
RafaSalgado <rasalgadol@unal.edu.co>
2018-11-05 08:03:22 -0500

    CORRECCION

--- src/students/server.js
+++ src/students/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.COURSES_URL
-export const port = process.env.COURSES_PORT
-export const entryPoint = process.env.COURSES_ENTRY
+export const url = process.env.STUDENTS_URL
+export const port = process.env.STUDENTS_PORT
+export const entryPoint = process.env.STUDENTS_ENTRY
